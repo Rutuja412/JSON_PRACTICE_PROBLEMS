66cb4550c0981541a3f54f30e58be851c608094e
Rutuja412 <rutujakadam4545@gmail.com>
2022-12-30 23:08:05 +0530

    [Rutuja]:Adding Buy and Sell

---
+++ StockInventoey/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace StockInventoey
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StockAccountManagement stockAccountManagement = new StockAccountManagement();
+
+            string stockFilePath = @"E:\RFP 232 BATCH\JSON_PRACTICE_PROBLEMS\StockInventoey\Stock.json";
+            stockAccountManagement.ReadStockJsonFile(stockFilePath);
+            string customerFilePath = @"E:\RFP 232 BATCH\JSON_PRACTICE_PROBLEMS\StockInventoey\Stock.json";
+            stockAccountManagement.ReadCustomerJsonFile(customerFilePath);
+            stockAccountManagement.BuyStock("Google");
+            stockAccountManagement.SellStocks("Amazon");
+            stockAccountManagement.WriteToStockJsonFile(stockFilePath);
+            stockAccountManagement.WriteToStockJsonFile(customerFilePath);
+
+        }
+    }
+}
---
+++ StockInventoey/StockDetails.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StockInventoey
+{
+    internal class StockDetails
+    {
+        public string StockName { get; set; }
+        public double StockPrice { get; set; }
+
+        public int NoOfShares { get; set; }
+    }
+}
